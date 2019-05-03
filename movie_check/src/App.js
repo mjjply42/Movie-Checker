3283ad6851e717f85327cfe678cbebcc747d00fd
Matthew <mjjply@Matthews-Air.attlocal.net>
2019-05-02 22:56:54 -0700

    first/test commit

---
+++ movie_check/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './header'
+import './styles/App.css';
+{/*import Movies from './movies.js'*/}
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      {/*<Movies />*/}
+    </div>
+  );
+}
+
+export default App;
---
+++ movie_check/src/header.js
@@ -0,0 +1,20 @@
+import React, { Component} from 'react'
+import './styles/header.css'
+
+class Header extends Component {
+
+
+
+    render()
+    {
+        return (
+        <div className="Head_container">
+            <div className="Header">
+                <h1> Hello Here is the header </h1>
+            </div>
+        </div>
+    )
+    }
+}
+
+export default Header
